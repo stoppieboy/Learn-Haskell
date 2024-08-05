ab9aee1d3a5ca481fca830c0abbf1ed2dd349673
Shivam Verma <shivamv1332@gmail.com>
2024-08-05 21:06:35 +0530

    Adding type signatures

--- HTML_printer_library/main.hs
+++ HTML_printer_library/main.hs
@@ -1,11 +1,26 @@
-html_ content = "<html>"<>content<>"</html>"
-body_ content = "<body>"<>content<>"</body>"
+html_::String->String
+-- html_ content = "<html>"<>content<>"</html>"
+html_ = el "html"
+
+body_::String->String
+-- body_ content = "<body>"<>content<>"</body>"
+body_ = el "body"
+
+head_::String->String
 head_ content = "<head>"<>content<>"</head>"
+
+title_::String->String
 title_ content = "<title>"<>content<>"</title>"
 
+makeHtml::String->String->String
 makeHtml titlec bodyc = html_ (head_ (title_ titlec) <> body_ bodyc)
 
-myhtml = makeHtml "Shivam's doc" "yeppp"
+-- myhtml::String
+-- myhtml = makeHtml "Shivam's doc" "yeppp"
+myhtml = makeHtml "Shivam's doc"
 
-main = do
-    putStrLn myhtml
+el::String->String->String
+el tag content =
+    "<"<>tag<>">"<>content<>"</"<>tag<>">"
+
+main = putStrLn (myhtml "yepp")
