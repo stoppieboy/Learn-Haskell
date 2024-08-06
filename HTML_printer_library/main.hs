3ec2a223d0963860a486700d9e7ceee7efefff44
Shivam Verma <shivamv1332@gmail.com>
2024-08-06 23:21:17 +0530

    Anonymous/Lambda functions

--- HTML_printer_library/main.hs
+++ HTML_printer_library/main.hs
@@ -7,15 +7,23 @@ body_::String->String
 body_ = el "body"
 
 head_::String->String
-head_ content = "<head>"<>content<>"</head>"
+-- head_ content = "<head>"<>content<>"</head>"
+head_ = el "head"
 
 title_::String->String
-title_ content = "<title>"<>content<>"</title>"
+-- title_ content = "<title>"<>content<>"</title>"
+title_ = el "title"
+
+p_::String->String
+p_ = el "p"
+
+h1_::String->String
+h1_ = el "h1"
 
 makeHtml::String->String->String
 makeHtml titlec bodyc = html_ (head_ (title_ titlec) <> body_ bodyc)
 
--- myhtml::String
+myhtml::String->String
 -- myhtml = makeHtml "Shivam's doc" "yeppp"
 myhtml = makeHtml "Shivam's doc"
 
@@ -23,4 +31,8 @@ el::String->String->String
 el tag content =
     "<"<>tag<>">"<>content<>"</"<>tag<>">"
 
-main = putStrLn (myhtml "yepp")
+-- normal function definition
+-- main = putStrLn (myhtml (h1_ "First Heading" <> p_ "First Para"))
+
+-- lambda function
+main = putStrLn ((\c1 c2 -> myhtml(h1_ c1 <> p_ c2)) "First Heading" "First Paragraph")
